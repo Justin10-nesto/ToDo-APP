a16cdb64417ad318f079c260611659d48d3b6b47
Justin Lasway <jastinlasway10@gmail.com>
2025-04-05 07:18:41 +0300

    Add docker compose down command to run-service.sh for proper service shutdown

--- run-service.sh
+++ run-service.sh
@@ -3,6 +3,8 @@ docker network create proxy
 
 touch acme.json && chmod 600 acme.json
 
+docker compose down
+
 docker compose up --build -d
 
 docker compose exec hotel python manage.py migrate --noinput
