1872a78a84ab00e2a93aad16bf9b1d4ab5b30740
Justin Lasway <jastinlasway10@gmail.com>
2025-04-07 10:20:18 +0300

    Update URL patterns in urls.py for improved API routing and organization

--- ApiDevt/urls.py
+++ ApiDevt/urls.py
@@ -2,36 +2,35 @@
 from django.urls import path
 from . import views
 from .views import *
-urlpatterns = [ 
+urlpatterns = [
+    path("index/", views.index, name="index"),
 
-    path('index/', views.index, name='index'),
+    path("v1/auth/login/", loginView.as_view(), name="login"),
+    path("v1/auth/logout/", logoutView.as_view(), name="logout"),
 
-    path('v1/auth/login/', loginView.as_view(),),
-    path('v1/auth/logout/', logoutView.as_view(),),
-    path('listUserMixin/', views.listUserMixin.as_view(), name='listUserMixin'),
-    path('listUser/', views.listUser, name='listUser'),
-    path('addUser/', views.addUser, name='addUser'),
-    path('showUser/<str:pk>/', views.showUser, name='showUser'),
-    path('updateUser/<str:pk>/', views.updateUser, name='updateUser'),
-    path('deleteUser/<str:pk>/', views.deleteUser, name='deleteUser'),
+    path("users/", views.listUserMixin.as_view(), name="list_user_mixin"),
+    path("users/list/", views.listUser, name="list_user"),
+    path("users/add/", views.addUser, name="add_user"),
+    path("users/<str:pk>/", views.showUser, name="show_user"),
+    path("users/<str:pk>/update/", views.updateUser, name="update_user"),
+    path("users/<str:pk>/delete/", views.deleteUser, name="delete_user"),
 
-    path('listOtpCode/', views.listOtpCode, name='listOtpCode'),
-    path('addOtpCode/', views.addOtpCode, name='addOtpCode'),
-    path('showOtpCode/<str:pk>/', views.showOtpCode, name='showOtpCode'),
-    path('updateOtpCode/<str:pk>/', views.updateOtpCode, name='updateOtpCode'),
-    path('deleteOtpCode/<str:pk>/', views.deleteOtpCode, name='deleteOtpCode'),
+    path("otp-codes/", views.listOtpCode, name="list_otp_code"),
+    path("otp-codes/add/", views.addOtpCode, name="add_otp_code"),
+    path("otp-codes/<str:pk>/", views.showOtpCode, name="show_otp_code"),
+    path("otp-codes/<str:pk>/update/", views.updateOtpCode, name="update_otp_code"),
+    path("otp-codes/<str:pk>/delete/", views.deleteOtpCode, name="delete_otp_code"),
 
-    path('listTaskCategory/', views.listTaskCategory, name='listTaskCategory'),
-    path('addTaskCategory/', views.addTaskCategory, name='addTaskCategory'),
-    path('showTaskCategory/<str:pk>/', views.showTaskCategory, name='showTaskCategory'),
-    path('updateTaskCategory/<str:pk>/', views.updateTaskCategory, name='updateTaskCategory'),
-    path('deleteTaskCategory/<str:pk>/', views.deleteTaskCategory, name='deleteTaskCategory'),
+    path("task-categories/", views.listTaskCategory, name="list_task_category"),
+    path("task-categories/add/", views.addTaskCategory, name="add_task_category"),
+    path("task-categories/<str:pk>/", views.showTaskCategory, name="show_task_category"),
+    path("task-categories/<str:pk>/update/", views.updateTaskCategory, name="update_task_category"),
+    path("task-categories/<str:pk>/delete/", views.deleteTaskCategory, name="delete_task_category"),
 
-    path('listTask/', views.listTask, name='listTask'),
-    path('addTask/', views.addTask, name='addTask'),
-    path('showTask/<str:pk>/', views.showTask, name='showTask'),
-    path('updateTask/<str:pk>/', views.updateTask, name='updateTask'),
-    path('deleteTask/<str:pk>/', views.deleteTask, name='deleteTask'),
-    path('taskNotification/', views.taskNotification, name='taskNotification'),
-
+    path("tasks/", views.listTask, name="list_task"),
+    path("tasks/add/", views.addTask, name="add_task"),
+    path("tasks/<str:pk>/", views.showTask, name="show_task"),
+    path("tasks/<str:pk>/update/", views.updateTask, name="update_task"),
+    path("tasks/<str:pk>/delete/", views.deleteTask, name="delete_task"),
+    path("tasks/notifications/", views.taskNotification, name="task_notification"),
 ]
--- TodoApp/urls.py
+++ TodoApp/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('ApiDevt.urls')),
+    path('api/', include('ApiDevt.urls')),
 ]
